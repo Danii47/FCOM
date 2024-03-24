9e44c76a9fd9f6f37066f25e7879da77b48050f6
Dani47 <87781904+Danii47@users.noreply.github.com>
2024-03-24 18:00:42 +0100

    feat: finish first prac

--- Prácticas/Practica1_FernandezVarona_DelPozoGomez_X9.asm
+++ Prácticas/Practica1_FernandezVarona_DelPozoGomez_X9.asm
@@ -12,6 +12,15 @@
 	A: .word 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
 	B: .space 40
 	C: .space 40
+	S1: .asciiz "Hola Uno Mundo Dos"
+	S2: .space 50
+	S3: .space 50
+	S4: .space 50
+	LineJump: .asciiz "\n"
+	S1Text: .asciiz "S1: "
+	S2Text: .asciiz "S2: "
+	S3Text: .asciiz "S3: "
+	S4Text: .asciiz "S4: "
 .text
 	main:
 		la $s0, A # Carga la dirección de A en $s0
@@ -60,27 +69,150 @@
 		lw $t1, ($s6) # Carga el valor de $s6 en $t1 (recorriendolo del derecho)
 		ble $t4, 5, AlternateValues
 	
+	
+	la $s6, S1      # Carga la dirección de S1
+	addi $s6, $s6, 17	# Apuntamos al final de la cadena
+	
+	la $s7, S2		# Carga la dirección de S2
+	
+	lb $t1, 0($s6)		# Carga en $t1 la cadena al revés
+	sb $t1, 0($s7)	
+	
+	reverseString:
+
+	
+	
+	
+		lb $t4, 0($s5)
+	
+		addi $s6, $s6, -1
+		addi $s7, $s7, 1	
+	
+		lb $t1, 0($s6)		#Carga en $t1 la cadena al revés
+		sb $t1, 0($s7)	
+	
+		bne $t1, $0, reverseString
+		
+
+	la $s6, S1
+	la $s0, S3
+	addi $t1, $0, 1 # Número de palabra recorrida en S1
+	lb $t3, 0($s6)
+	
+	SaveOddS3:
+		
+		
+		
+		beq $t3, 32, AddWordS1
+
+		andi $t4, $t1, 1
+		bne $t4, 1, ContinueS3
+		
+		sb $t3, 0($s0)
+		addi $s0, $s0, 1
+		
+		ContinueS3:
+			addi $s6, $s6, 1
+			lb $t3, 0($s6)
+			bne $t3, $0, SaveOddS3
+	
+	
+	la $s6, S2
+	la $s0, S4
+	addi $t1, $0, 1 # Número de palabra recorrida en S2
+	lb $t3, 0($s6)
+	
+	SaveOddS4:
+	
+		
+		
+		beq $t3, 32, AddWordS2
+
+		andi $t4, $t1, 1
+		bne $t4, 1, ContinueS4
+		
+		sb $t3, 0($s0)
+		addi $s0, $s0, 1
+	
+		ContinueS4:
+			addi $s6, $s6, 1
+			lb $t3, 0($s6)
+			bne $t3, $0, SaveOddS4
+		
+	la $a0, S1Text
+	li $v0, 4
+	syscall
+	
+	la $a0, S1
+	li $v0, 4
+	syscall
+	
+	la $a0, LineJump
+	li $v0, 4
+	syscall
+	
+	la $a0, S2Text
+	li $v0, 4
+	syscall
+
+
+	la $a0, S2
+	li $v0, 4
+	syscall
+	
+	la $a0, LineJump
+	li $v0, 4
+	syscall
+	
+	la $a0, S3Text
+	li $v0, 4
+	syscall
+	
+	la $a0, S3
+	li $v0, 4
+	syscall
+
+	la $a0, LineJump
+	li $v0, 4
+	syscall
+	
+	la $a0, S4Text
+	li $v0, 4
+	syscall
+	
+	la $a0, S4
+	li $v0, 4
+	syscall
+	
 	li $v0 10 # Cierra el programa
 	syscall
-		
-	ACopy:
-		sw $t0 ($s4)
-		addi $s4, $s4, 4
-		j LoopContinuation
 	
-	BCopy:
-		sw $t0 ($s5)
-		addi $s5, $s5, 4
-		j LoopContinuation
+AddWordS1:
+	addi $t1, $t1, 1
+	j ContinueS3
+	
+AddWordS2:
+	addi $t1, $t1, 1
+	j ContinueS4
+	
+ACopy:
+	sw $t0 ($s4)
+	addi $s4, $s4, 4
+	j LoopContinuation
+	
+BCopy:
+	sw $t0 ($s5)
+	addi $s5, $s5, 4
+	j LoopContinuation
 		
-	AlternateValues:
-		sw $t0, ($s6)
-		sw $t1, ($s0)
-		addi $s6, $s6, 4
-		addi $s0, $s0, -4
-		addi $t4, $t4, 1
+AlternateValues:
+	sw $t0, ($s6)
+	sw $t1, ($s0)
+	addi $s6, $s6, 4
+	addi $s0, $s0, -4
+	addi $t4, $t4, 1
 		
-		j AlternateValuesLoop
+	j AlternateValuesLoop
 		
 # A la vista de lo observado en el paso anterior: ¿Cuál es el propósito del programa? ¿Cuántas iteraciones
 # realiza? Cuando se observa la consola (en la parte inferior, pestaña Run I/O) ¿Por qué el programa no
